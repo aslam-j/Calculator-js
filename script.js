212d1ab6156165e615e7f6e8fb85f2c65aed46d0
aslam <jamalaslam020@gmail.com>
2025-06-26 18:37:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let input = document.querySelector("input")
+
+function addvalue(elementval){
+    input.value += elementval
+
+}
+
+function clearval(){
+    input.value = ""
+}
+
+function deleteval(){
+    input.value = input.value.slice(0,input.value.length-1)
+}
+
+function evaluvateval(){
+    input.value = eval(input.value)
+}
